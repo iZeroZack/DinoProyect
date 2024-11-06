add336f99ed0bba0918227383fbcabcaf41c9460
mario <mario.jorquera.11@gmail.com>
2024-11-06 10:08:21 -0300

    fix

--- Clases/Juego.py
+++ Clases/Juego.py
@@ -6,44 +6,38 @@ import ObstaculoFactory
 # Inicializa Pygame
 pg.init()
 
-# Configura el modo de video (pantalla)
+
 ANCHO = 800
 ALTO = 600
 VENTANA = pg.display.set_mode((ANCHO, ALTO))
 
-# Ahora ya está bien configurada la ventana y podemos proceder con la carga de imágenes
+
+
+# Creación de la arreglo animacion
+sheet = pg.image.load("move.png").convert_alpha()
 animacion_move = []
 
-# Manejo de errores para cargar la imagen
-
-sheet = pg.image.load("move.png").convert_alpha()
-
-# Creación de la animación
-frame_width = 24  # Ancho original del frame
-frame_height = 24 # Alto original del frame
-scale_factor = 5  # Factor de escala
+frame_width = 24
+frame_height = 24
+escala_factor = 5
 num_columnas = 6
 for column in range(num_columnas):
     animation = pg.Rect(frame_width * column, 0, frame_width, frame_height)
     frame = sheet.subsurface(animation)
-    enlarged_frame = pg.transform.scale(frame, (frame_width * scale_factor, frame_height * scale_factor))
+    enlarged_frame = pg.transform.scale(frame, (frame_width * escala_factor, frame_height * escala_factor))
     animacion_move.append(enlarged_frame)
-
-# Inicialización de variables y objetos
+########
 GRAVEDAD = 2
 jugando = True
 
-# Asegúrate de que las clases estén correctamente definidas
 dragon = Dragon.Dragon(100, 100, animacion_move)
 terreno = Terreno.Terreno(ANCHO, ALTO)
 
-# Creación de obstáculos
 obstaculos = []
 fabrica = ObstaculoFactory.ObstaculoFactory()
 pino = fabrica.crear_obstaculo_terrestre(terreno.getAlto(), "Bosque")
 obstaculos.append(pino)
 
-# Función para gestionar las teclas
 def gestionarTeclas(teclas):
     if teclas[pg.K_UP]:
         dragon.y -= dragon.velocidad + 4
@@ -52,7 +46,6 @@ def gestionarTeclas(teclas):
     if teclas[pg.K_RIGHT]:
         dragon.x += dragon.velocidad
 
-# Bucle principal
 while jugando:
     eventos = pg.event.get()
     teclas = pg.key.get_pressed()
